87a034bc63701f52b9c454c393d994d3872a41a4
MOHAMMAD KHUBAIB SARKER <mohammad@kali.kali>
2024-03-25 02:55:25 +0600

    Custom Tailwind class and font setup

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,14 @@
+import Navbar from "../Shared/Navbar/Navbar";
+import Header from "../Shared/header/Header";
+
 const Home = () => {
     return (
         <div>
-            <h2 className="text-3xl">This is home</h2>
+            <Header></Header>
+            <Navbar></Navbar>
+            <h2 className="text-3xl font-poppins font-extrabold">
+                This is home
+            </h2>
         </div>
     );
 };
---
+++ src/pages/Shared/LeftSideNave/LeftSideNave.jsx
@@ -0,0 +1,9 @@
+const LeftSideNave = () => {
+    return (
+        <div>
+            <h2 className="text-2xl">Left Side nav</h2>
+        </div>
+    );
+};
+
+export default LeftSideNave;
