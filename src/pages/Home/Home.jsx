50ba4f0577e94f1311cc1e084ec42901b53f87b8
MOHAMMAD KHUBAIB SARKER <mohammad@kali.kali>
2024-03-25 14:01:27 +0600

    Home page layout and date time formatting using Moment.Js

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,6 @@
+import LeftSideNave from "../Shared/LeftSideNave/LeftSideNave";
 import Navbar from "../Shared/Navbar/Navbar";
+import RightSideNav from "../Shared/RightSideNav/RightSideNav";
 import Header from "../Shared/header/Header";
 
 const Home = () => {
@@ -9,6 +11,17 @@ const Home = () => {
             <h2 className="text-3xl font-poppins font-extrabold">
                 This is home
             </h2>
+            <div className="grid grid-cols-1 md:grid-cols-4 gap-6 border">
+                <div className="border">
+                    <LeftSideNave></LeftSideNave>
+                </div>
+                <div className="border md:col-span-2">
+                    <h2 className="text-4xl">news coming soon</h2>
+                </div>
+                <div className="border">
+                    <RightSideNav></RightSideNav>
+                </div>
+            </div>
         </div>
     );
 };
